fa93797792d2ec3e6dff9df87c0ea6284dbdebe2
MohammadShahheydar <m.shahheydar110>
2024-06-05 18:43:48 +0330

    feat: update todo card

--- src/features/todoList/ui/component/TodoCard.tsx
+++ src/features/todoList/ui/component/TodoCard.tsx
@@ -2,26 +2,27 @@ import React from 'react';
 import styles from "@/features/todoList/ui/cssModule/TodoCard.module.css"
 import Button from "@/kit/button/Button";
 import {ListTodo, Pencil, Trash2} from "lucide-react";
+import Card from "@/kit/card/Card";
 
 const TodoCard = () => {
     return (
-        <div className={`${styles.TodoCard} ${styles.bgGradient}`}>
+        <Card bg={'gradient'}>
             <div className={styles.title}>
                 <h4>عنوان</h4>
                 <p>توضیحات</p>
             </div>
             <div className={styles.actions}>
                 <Button variant={'success'}>
-                    <ListTodo size={20} strokeWidth={1} />
+                    <ListTodo size={20} strokeWidth={1}/>
                 </Button>
                 <Button variant={'info'}>
-                    <Pencil size={20} strokeWidth={1} />
+                    <Pencil size={20} strokeWidth={1}/>
                 </Button>
                 <Button variant={'danger'}>
-                    <Trash2 size={20} strokeWidth={1} />
+                    <Trash2 size={20} strokeWidth={1}/>
                 </Button>
             </div>
-        </div>
+        </Card>
     );
 };
 
