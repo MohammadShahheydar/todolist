05df1d2210de84ede13e992d369ea95a82b03617
MohammadShahheydar <m.shahheydar110>
2024-06-05 21:37:49 +0330

    feat: add home link

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import PageGreeting from "@/kit/pageGreeting/PageGreeting";
 import AddTodoLayout from "@/features/addTodo/ui/layout/AddTodoLayout";
+import Link from "next/link";
+import Button from "@/kit/button/Button";
+import {Home} from "lucide-react";
 
 const Page = () => {
     return (
-        <PageGreeting title={'تسک جدید'}>
+        <PageGreeting
+            title={'تسک جدید'}
+            actions={
+                <Link href={'/'}>
+                    <Button variant={'success'}>
+                        <Home size={20} strokeWidth={1}/>
+                    </Button>
+                </Link>
+            }
+        >
             <AddTodoLayout/>
         </PageGreeting>
     );
