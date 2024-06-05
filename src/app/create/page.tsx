ebf8d5157e66151f690382f04606f28f37e5cef0
MohammadShahheydar <m.shahheydar110>
2024-06-05 15:37:20 +0330

    feat: init add task page

---
+++ src/app/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageGreeting from "@/kit/pageGreeting/PageGreeting";
+
+const Page = () => {
+    return (
+        <PageGreeting title={'تسک جدید'}>
+            <h4>hi</h4>
+        </PageGreeting>
+    );
+};
+
+export default Page;
