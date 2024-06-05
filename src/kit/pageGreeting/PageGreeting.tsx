41ccb9906e52e887f51933d38a55936a810e9aff
MohammadShahheydar <m.shahheydar110>
2024-06-05 15:52:37 +0330

    feat: add actions to PageGreeting

--- src/kit/pageGreeting/PageGreeting.tsx
+++ src/kit/pageGreeting/PageGreeting.tsx
@@ -1,10 +1,13 @@
 import React, {ReactNode} from 'react';
 import styles from "@/kit/pageGreeting/PageGreeting.module.css";
 
-const PageGreeting = ({children , title} : {children: ReactNode , title: string}) => {
+const PageGreeting = ({children , title , actions} : {children: ReactNode , title: string , actions?: ReactNode}) => {
     return (
         <div className={styles.listLayout}>
-            <h2 className={styles.listHeader}>{title}</h2>
+            <div className={styles.listHeaderWrapper}>
+                <h2 className={styles.listHeader}>{title}</h2>
+                {actions}
+            </div>
             {children}
         </div>
     );
